75693caef58d216a0078784a0e0a69c5e08c05ac
QNeee <vovasagan7@gmail.com>
2022-12-12 20:48:31 +0300

    asdsaa

--- src/js/services/localStorage.js
+++ src/js/services/localStorage.js
@@ -6,7 +6,7 @@
  * favoriteIngredientsMarkup.map(......)
  * example!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  */
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const THEME_KEY = 'theme';
 const FAVORITE_COCKTAILS = 'favoriteCocktails';
 const FAVORITE_INGREDIENTS = 'favoriteIngredients';
@@ -24,12 +24,17 @@ class LocalStorage {
     setFavoriteIngredients(data) {
         const favoriteIngredients = this.getFavoriteIngredients();
         const ingredients = favoriteIngredients.find(ingredients => ingredients.id === data.id);
-        if (ingredients) {
-            return Notify.failure('This ingredient is already in favorite');
-        }
-        favoriteIngredients.push(data);
-        const stringifiedData = JSON.stringify(favoriteIngredients);
-        localStorage.setItem(FAVORITE_INGREDIENTS, stringifiedData);
+        if (Array.isArray(data) && !this.hasIngredientsInFavorite(ingredients)) {
+            favoriteIngredients.push(data);
+            const stringifiedData = JSON.stringify(...favoriteIngredients);
+            localStorage.setItem(FAVORITE_INGREDIENTS, stringifiedData);
+        } else
+            if (!ingredients) {
+                favoriteIngredients.push(data);
+                const stringifiedData = JSON.stringify(favoriteIngredients);
+                localStorage.setItem(FAVORITE_INGREDIENTS, stringifiedData);
+            }
+
     }
 
     getFavoriteCocktails() {
@@ -39,13 +44,58 @@ class LocalStorage {
     setFavoriteCocktails(data) {
         const favoriteCocks = this.getFavoriteCocktails();
         const cocks = favoriteCocks.find(cocks => cocks.id === data.id);
-        if (cocks) {
-            return Notify.failure('This cocktail is already in favorite');
-        }
-        favoriteCocks.push(data);
-        const stringifiedData = JSON.stringify(favoriteCocks);
-        localStorage.setItem(FAVORITE_COCKTAILS, stringifiedData);
-    }
+        if (Array.isArray(data) && !this.hasCocktailsInFavorite(cocks)) {
+            favoriteCocks.push(data);
+            const stringifiedData = JSON.stringify(...favoriteCocks);
+            localStorage.setItem(FAVORITE_COCKTAILS, stringifiedData);
+        } else
+            if (!cocks) {
+                favoriteCocks.push(data);
+                const stringifiedData = JSON.stringify(favoriteCocks);
+                localStorage.setItem(FAVORITE_COCKTAILS, stringifiedData);
+            }
 
+    }
+    removeFavoriteCocktails(id) {
+        const favoriteCocks = this.getFavoriteCocktails();
+        const index = favoriteCocks.findIndex(n => n.id === id);
+
+        if (index !== -1) {
+            favoriteCocks.splice(index, 1);
+            localStorage.removeItem(FAVORITE_COCKTAILS);
+            this.setFavoriteCocktails(favoriteCocks);
+        }
+
+    }
+    removeFavoriteIngredients(id) {
+        const favoriteIngredients = this.getFavoriteIngredients();
+        const index = favoriteIngredients.findIndex(n => n.id === id);
+
+        if (index !== -1) {
+            favoriteIngredients.splice(index, 1);
+            localStorage.removeItem(FAVORITE_INGREDIENTS);
+            this.setFavoriteIngredients(favoriteIngredients);
+        }
+
+    }
+    hasCocktailsInFavorite(id) {
+        const favoriteCocks = this.getFavoriteCocktails();
+        const hasIdCock = favoriteCocks.find(item => item.id === id);
+        if (hasIdCock) {
+            true;
+        } else {
+            false;
+        }
+    }
+    hasIngredientsInFavorite(id) {
+        const favoriteIngredients = this.getFavoriteCocktails();
+        const hasIdIngredient = favoriteIngredients.find(item => item.id === id);
+        if (hasIdIngredient) {
+            true;
+        } else {
+            false;
+        }
+    }
 }
+
 export default LocalStorage;
