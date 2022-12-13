889041b66c40f080d53e1c8ff2f3262d3b91d4d4
DShpak27 <mr.shpak@gmail.com>
2022-12-13 09:41:20 +0200

    123

--- src/index.js
+++ src/index.js
@@ -47,6 +47,7 @@ import CocktailDetailsContent from './js/modal/CocktailDetailsContent';
 import CocktailAPI from './js/services/cocktailsAPI';
 import Modal from './js/modal/Modal';
 
+
 document.querySelector('.test').addEventListener('click', () => {
   const f = new CocktailAPI();
 
