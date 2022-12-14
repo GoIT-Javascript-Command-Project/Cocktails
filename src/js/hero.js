2febf3facb0be7d6e34137ad5aef1ddf2d1dd3da
DaniilaSaenko <daniila.s777@gmail.com>
2022-12-14 09:21:32 +0200

    creat hero

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,20 +1,19 @@
-
 import { getCocktailsByFirstLetter } from './services/cocktailAPI';
-
-
-
 
 const {
   heroButtonRef,
-  heroSelectRef,
   letterList,
+  inputMobile,
+  inputSpan,
+  selectLetter,
 } = {
   heroButtonRef: document.querySelector('.hero__list'),
-  heroSelectRef: document.querySelector('[name="search"]'),
   letterList: document.querySelector('.hero__list'),
 
+  inputMobile: document.querySelector('.select__input'),
+  inputSpan: document.querySelector('.input-span'),
+  selectLetter: document.querySelector('#select'),
 };
-
 
 const buttons = [
   'A',
@@ -55,17 +54,46 @@ const buttons = [
   '0',
 ];
 
+
 onLoadingHero();
 letterList.addEventListener('click', e => onClickSearchLetter(e));
-heroSelectRef.addEventListener('change', e => letterListMob(e));
+inputMobile.addEventListener('click', onMobLetterClick);
 
 
+//
+
+let checked = document.querySelector('.select__input');
+let mobOpen = document.querySelector('.gallery__title');
+
+
+
+// Функция создания кнопок поиска для десктоп и таблет
 function onLoadingHero() {
   createSearchButtons(buttons);
-  createSearchButtonsMobile(buttons);
-
 }
 
+async function onClickSearchLetter(e) {
+  if (e.target.nodeName === 'BUTTON') {
+    try {
+      const letter = e.target.textContent;
+      console.log(letter);
+      const response = await getCocktailsByFirstLetter(letter);
+    } catch (error) {}
+  }
+}
+
+async function onLetterClick(e) {
+  checked.classList.add('select__input-checked');
+  if (!e.target.dataset.letter) return;
+  try {
+    const letter = e.target.textContent;
+    console.log(letter);
+      const response = await getCocktailsByFirstLetter(letter);
+  } catch (error) {}
+}
+
+
+// Функция создания (разметка) кнопок поиска для десктоп и таблет
 function createSearchButtons(buttons) {
   const markup = buttons
     .map(button => {
@@ -75,35 +103,40 @@ function createSearchButtons(buttons) {
   heroButtonRef.innerHTML = markup;
 }
 
-function createSearchButtonsMobile(buttons) {
-  const markup = buttons
-    .map(button => {
-      return `<option value="${button}">${button}</option>`;
+// Функция создания (разметка) списка поиска для мобилки
+function createDroplist() {
+  const markup = `<div class="select__dropdown">
+      <ul class="select__list">
+      ${createButtonsMarkup()}
+      </ul>
+      </div>`;
+  selectLetter.insertAdjacentHTML('beforeend', markup);
+}
+
+function createButtonsMarkup() {
+  return buttons
+    .map(btn => {
+      return `
+      <li class="select__item" data-letter="${btn}">${btn.toUpperCase()}</li>
+      `;
     })
     .join('');
-  heroSelectRef.innerHTML = markup;
 }
-
-async function letterListMob(e) {
-  if (e.target.nodeName === 'SELECT') {
-    try {
-      const letter = e.target.value;
-      console.log(letter);
-      const response = await getCocktailsByFirstLetter(letter);
-    } catch (error) {
-    }
+// Функция создания вслывающего списка
+function onMobLetterClick() {
+  mobOpen.classList.add('mob-open');
+  if (selectLetter.children.length > 1) {
+    mobOpen.classList.remove('mob-open');
+    selectLetter.lastChild.remove();
+    return;
   }
-}
 
-async function onClickSearchLetter(e) {
-  if (e.target.nodeName === 'BUTTON') {
-    try {
-      const letter = e.target.textContent;
-      console.log(letter)
-      const response = await getCocktailsByFirstLetter(letter);
-      console.dir(response);
-    } catch (error) {
-    }
-  }
+  createDroplist();
+  selectLetter.lastChild.addEventListener('click', e => {
+    onLetterClick(e);
+    inputSpan.textContent = e.target.dataset.letter.toUpperCase();
+    selectLetter.lastChild.remove();
+    mobOpen.classList.remove('mob-open');
+  });
 }
 
