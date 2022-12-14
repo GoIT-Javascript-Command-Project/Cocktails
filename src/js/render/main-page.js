30faf61aa3c5881e689dbd34eb3dbc8ccc36b848
Lysak-Anna <hvlysak@gmail.com>
2022-12-14 13:55:50 +0300

    my update

--- src/js/render/main-page.js
+++ src/js/render/main-page.js
@@ -1,29 +1,44 @@
-import CocktailsAPI from '../services/cocktailsAPI';
-
+// import CocktailsAPI from '../services/cocktailsAPI';
 import CocktailCard from './../CocktailCard';
+import mainPage from '../../templates/main-page.hbs';
 const cardList = document.querySelector('.pagination__cards');
 
-class RenderMainPage {
-  constructor() {
-    this.data;
+export default class RenderMainPage {
+  constructor(data) {
+    this.data = data;
   }
-
-  async render() {
-    this.data = await CocktailsAPI.getRandomCocktails(9);
-    if (
-      window.matchMedia('(min-width: 768px) and (max-width: 1279.98px)').matches
-    ) {
-      this.data.length = 6;
-    } else if (window.matchMedia('(max-width: 767.98px)').matches) {
-      this.data.length = 3;
-    }
-    cardList.innerHTML = '';
+  render(data) {
+    const section = document.createElement('section');
+    const template = mainPage();
+    section.innerHTML = template;
     cardList.append(
       ...this.data.map(item => {
         return new CocktailCard(item).render();
       })
     );
+    return section;
   }
 }
 
-export default RenderMainPage;
+// class RenderMainPage {
+//   constructor() {
+//     this.data;
+//   }
+
+//   async render() {
+//     this.data = await CocktailsAPI.getRandomCocktails(9);
+//     if (
+//       window.matchMedia('(min-width: 768px) and (max-width: 1279.98px)').matches
+//     ) {
+//       this.data.length = 6;
+//     } else if (window.matchMedia('(max-width: 767.98px)').matches) {
+//       this.data.length = 3;
+//     }
+//     cardList.innerHTML = '';
+//     cardList.append(
+//       ...this.data.map(item => {
+//         return new CocktailCard(item).render();
+//       })
+//     );
+//   }
+// }
--- src/js/render/search-page.js
+++ src/js/render/search-page.js
@@ -1,26 +1,42 @@
 import Pagination from '../pagination/pagination';
-import CocktailsAPI from '../services/cocktailsAPI';
-
-const form = document.querySelector('form');
+import searchPage from '../../templates/search-page.hbs';
 
 export default class RenderSearchPage {
-  constructor() {
-    this.searchWord = '';
-    this.data;
+  constructor(data) {
+    this.data = data;
   }
-  render() {
-    this.getWord();
-  }
-  getWord() {
-    form.addEventListener('submit', async event => {
-      event.preventDefault();
-      this.searchWord = event.currentTarget.elements.cocktail.value;
-      this.data = await CocktailsAPI.getCocktailsByName(this.searchWord);
-      this.createPagination();
-    });
-  }
-  createPagination() {
-    const pagination = new Pagination(this.data, 3);
+  render(data) {
+    const section = document.createElement('section');
+    const markUp = searchPage();
+    section.innerHTML = markUp;
+    const pagination = new Pagination(this.data, 9);
     pagination.init();
+    return section;
   }
 }
+
+// import CocktailsAPI from '../services/cocktailsAPI';
+
+// const form = document.querySelector('form');
+
+// export default class RenderSearchPage {
+//   constructor() {
+//     this.searchWord = '';
+//     this.data;
+//   }
+//   render() {
+//     this.getWord();
+//   }
+//   getWord() {
+//     form.addEventListener('submit', async event => {
+//       event.preventDefault();
+//       this.searchWord = event.currentTarget.elements.cocktail.value;
+//       this.data = await CocktailsAPI.getCocktailsByName(this.searchWord);
+//       this.createPagination();
+//     });
+//   }
+//   createPagination() {
+//     const pagination = new Pagination(this.data, 3);
+//     pagination.init();
+//   }
+// }
